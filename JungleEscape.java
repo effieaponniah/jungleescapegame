f372a6f4815b5530d16529e5188be71d1f3b9d9a
Effiea Ponniah <32806517+effieaponniah@users.noreply.github.com>
2021-02-07 15:35:34 -0500

    Update JungleEscape.java

--- JungleEscape.java
+++ JungleEscape.java
@@ -1,5 +1,16 @@
 /*
-   This program is written by Effiea Ponniah on May 2019
+   Written by Effiea Ponniah (effiea.com)
+   Created on May 14, 2019
+   Last Updated on February 7, 2021
+   This game is based off the board game, snakes and ladders. 
+   There are two levels and if the user passes the first level he or she can move onto the next level. 
+   The user will verse the computer. User's game piece will be a light blue (cyan) circle while, a 
+   computer's game piece is magenta. There will be a randomly generated dice which will give the 
+   number of tiles to move. The first roll of the user and computer will determind who can start the game. 
+   The winner will be determined by who rolls the higher number. If the computer or user rolls 6, 
+   they will be asked to roll again. The sum of all the rolls will give how much the computer or user will move. 
+   The user or computer will go until 25. The winner will rech 25 or a number higher than 25. 
+   If computer wins level one will be played again or user will have the option to exit the game.
 */
 
 import hsa.Console;
